8e7658a35f93e9c52811f1503bd855ebd31b2f0f
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2022-04-14 20:39:44 -0700

    new icon

--- iSuite/Resources.Designer.cs
+++ iSuite/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace iSuite {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         public static System.Drawing.Bitmap usb {
